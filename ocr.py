0c45a1273fcd864c2f7d82c61a5afd87a99f469d
Shohdy ELSheemy <shohdi@gmail.com>
2024-03-30 06:57:16 +0300

    finish code and working with cloud

--- ocr.py
+++ ocr.py
@@ -22,6 +22,8 @@ import time
 import tkinter as tk
 from tkinter import filedialog
 import json
+from google.cloud import storage
+from google.cloud import vision
 
 
 class StoppableThread(threading.Thread):
@@ -40,6 +42,12 @@ class StoppableThread(threading.Thread):
         return self._stop_event.is_set()
 
 
+def sort_blob(blob):
+    """Sorts blobs based on the numerical part of their name."""
+    
+    return f"{int(re.search('output-([0-9]+)-', blob.name).group(1)):0>{4}}"
+
+
 def getPdfJsonFiles(pdf_file):
     ret = []
     with open('sample.json', 'r',encoding='utf-8') as f:
@@ -49,6 +57,147 @@ def getPdfJsonFiles(pdf_file):
 
     return ret
     
+
+def load_config(config_file_path):
+    with open(config_file_path, 'r') as f:
+        config = json.load(f)
+    return config
+
+def upload_to_bucket( file_path, destination_blob_name):
+    """Uploads a file to a Google Cloud Storage bucket."""
+    pathDir,pathName = os.path.split(destination_blob_name)
+    config_file_path = "config/config.json" 
+    config = load_config(config_file_path)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config["service_account_key_path"]
+    bucket_name = config["bucket_name"]
+    storage_client = storage.Client(project=config["project_id"])
+    bucket = storage_client.bucket(bucket_name)
+    
+    blob_list = [
+        blob.name
+        for blob in list(bucket.list_blobs(prefix=pathDir))
+        if not blob.name.endswith("/")
+    ]
+    
+    if destination_blob_name in blob_list:
+        return
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(file_path)
+
+    print(f"File {file_path} uploaded to {destination_blob_name} in bucket {bucket_name}.")
+
+
+def async_detect_document(gcs_source_uri, gcs_destination_uri):
+    """OCR with PDF/TIFF as source files on GCS"""
+    
+    
+    config_file_path = "config/config.json" 
+    config = load_config(config_file_path)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config["service_account_key_path"]
+    bucket_name = config["bucket_name"]
+    storage_client = storage.Client(project=config["project_id"])
+    bucket = storage_client.get_bucket(bucket_name)
+    # List objects with the given prefix, filtering out folders.
+    blob_list = None
+    try:
+        blob_list = [
+            blob
+            for  blob in list(bucket.list_blobs(prefix=gcs_destination_uri))
+            if not blob.name.endswith("/")
+        ]
+    except Exception as ex:
+        blob_list = None
+
+
+    if blob_list is None or len(blob_list) == 0:
+
+        # Supported mime_types are: 'application/pdf' and 'image/tiff'
+        mime_type = "application/pdf"
+
+        # How many pages should be grouped into each json output file.
+        batch_size = 1
+
+        client = vision.ImageAnnotatorClient()
+        
+        feature = vision.Feature(type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
+        sourcePath  = "gs://" + bucket_name + "/" + gcs_source_uri
+        destPath  = "gs://" + bucket_name + "/" + gcs_destination_uri
+        gcs_source = vision.GcsSource(uri=sourcePath)
+        input_config = vision.InputConfig(gcs_source=gcs_source, mime_type=mime_type)
+
+        gcs_destination = vision.GcsDestination(uri=destPath)
+        output_config = vision.OutputConfig(
+            gcs_destination=gcs_destination, batch_size=batch_size
+        )
+
+        async_request = vision.AsyncAnnotateFileRequest(
+            features=[feature], input_config=input_config, output_config=output_config
+        )
+
+        operation = client.async_batch_annotate_files(requests=[async_request])
+
+        print("Waiting for the operation to finish.")
+        operation.result(timeout=420)
+
+        # Once the request has completed and the output has been
+        # written to GCS, we can list all the output files.
+        
+
+        
+        
+        prefix = gcs_destination_uri
+
+        
+
+        # List objects with the given prefix, filtering out folders.
+        blob_list = [
+            blob
+            for  blob in list(bucket.list_blobs(prefix=prefix))
+            if not blob.name.endswith("/")
+        ]
+        
+        print("Output files:")
+    out_str = ''
+    blob_list.sort(key=sort_blob)
+    for blob in blob_list:
+        print(blob.name)
+
+        # Process the first output file from GCS.
+        # Since we specified batch_size=2, the first response contains
+        # the first two pages of the input file.
+        output = blob
+
+        json_string = output.download_as_bytes().decode("utf-8")
+        response = json.loads(json_string)
+
+        # The actual response for the first page of the input file.
+        first_page_response = response["responses"][0]
+        if first_page_response is not None and "fullTextAnnotation" in first_page_response :
+            annotation = first_page_response["fullTextAnnotation"]
+
+            # Here we print the full text from the first page.
+            # The response contains more information:
+            # annotation/pages/blocks/paragraphs/words/symbols
+            # including confidence scores and bounding boxes
+            #print("Full text:\n")
+            #print(annotation["text"])
+            if annotation is not None and "text" in annotation :
+                out_str = out_str + getJsonHtmlFromStr(annotation["text"])
+    return out_str
+        
+        
+
+
+
+def getJsonHtmlFromStr(jsonStr):
+    myExtractedText = re.sub('[\s]','&nbsp;',jsonStr)
+    myExtractedText = re.sub('[\t]','&nbsp;&nbsp;&nbsp;',myExtractedText)
+    myExtractedText = re.sub('[\n]+','<br />',myExtractedText)
+    
+    
+    myExtractedText =  "<p>" +   myExtractedText + "</p>"
+    return myExtractedText
 
 
 spellChecker = None
@@ -66,8 +215,8 @@ def pdf_to_txt(pdf_file,allowSpell,rootWindow):
     global lblPageNo
     
     pathDir,pathName = os.path.split(pdf_file)
-    pathName = pathName + "_ext"
-    extFullPath = pathDir + os.path.sep + pathName
+    pathNameExt = pathName + "_ext"
+    extFullPath = pathDir + os.path.sep + pathNameExt
     lblStatus.config(text=extFullPath)
     
     print(extFullPath)
@@ -109,11 +258,20 @@ def pdf_to_txt(pdf_file,allowSpell,rootWindow):
     
     #imageNames =  os.listdir(extFullPath)
     #imageNames.sort()
+
+
+    # Example usage:
     
+    file_path = pdf_file
+    destination_blob_name = "books/" + pathName  # Optional - include a folder path if desired
+
+    upload_to_bucket( file_path, destination_blob_name)
+    innerDoc = async_detect_document(destination_blob_name,'books/' + pathName + '/out/')
     indx = 1
-    files = getPdfJsonFiles(pdf_file)
+    #files = getPdfJsonFiles(pdf_file)
     out_str = '<!DOCTYPE html><html lang="ar" dir="rtl"><head><meta charset="utf-8" /></head><body>'
 
+    '''
     for jsonPage in files:
         if myThread.stopped():
             return
@@ -130,10 +288,12 @@ def pdf_to_txt(pdf_file,allowSpell,rootWindow):
                     
                     
                     out_str = out_str + "<p>" +   myExtractedText + "</p>"
+
         
 
         indx+=1
-
+    '''
+    out_str = out_str + innerDoc
 
     #out_str = re.sub('[\n]+','<br />',out_str)        
     out_str = out_str + '</body></html>'
@@ -300,10 +460,14 @@ if __name__ == '__main__':
     btnUpload.pack()
     lblFileName = tk.Label(rootWindow,font=("Ariel",16),text=filenamePrm,wraplength=600)
     lblFileName.pack()
+    
+    '''
     chkDoSpellCheckVal = tk.BooleanVar(value=allowSpellPrm)
+
     
     chkDoSpellCheck =  tk.Checkbutton(rootWindow, text='اصلح الاخطاء',variable=chkDoSpellCheckVal, onvalue=True, offvalue=False, command=lambda:chkDoSpellCheck_click())
     chkDoSpellCheck.pack()
+    '''
     lblPageNo = tk.Label(rootWindow,font=("Ariel",16),text='',wraplength=600)
     lblPageNo.pack()
     lblStatus = tk.Label(rootWindow,font=("Ariel",16),text='',wraplength=600)
@@ -319,16 +483,18 @@ if __name__ == '__main__':
     txtInput = tk.Text(rootWindow,font=("Ariel",16),height=1)
     txtInput.pack()
 
-
+    '''
     btnCorrect = tk.Button(rootWindow,
                    text = "صحح",
                    command = lambda:btnCorrect_click())
     btnCorrect.pack()
-
+    '''
+    '''
     btnIgnore = tk.Button(rootWindow,
                    text = "اهمل",
                    command = lambda:btnIgnore_click())
     btnIgnore.pack()
+    '''
 
     tk.Button(rootWindow,
                    text = "ابدأ",
